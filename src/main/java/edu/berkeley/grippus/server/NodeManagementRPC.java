27ecc729f8c06cf3bf6ed9f23d87b146e345db28
Steven Schlansker <stevenschlansker@berkeley.edu>
2009-11-03 10:25:38 -0800

    Clean up whitespace

--- src/main/java/edu/berkeley/grippus/server/NodeManagementRPC.java
+++ src/main/java/edu/berkeley/grippus/server/NodeManagementRPC.java
@@ -4,7 +4,6 @@ import edu.berkeley.grippus.Errno;
 import edu.berkeley.grippus.fs.DFileSpec;
 
 public interface NodeManagementRPC {
-
 	public String version(String cmd);
 	public String terminate(String cmd);
 	public String status(String cmd);
