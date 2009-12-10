a9bdbb1514668699e64cfd1ee26b924df206ac17
Steven Schlansker <steven@sexy.stevenschlansker.is-a-geek.org>
2009-12-10 12:25:25 -0800

    Remove unused import

--- src/main/java/edu/berkeley/grippus/storage/LocalFilesystemStorage.java
+++ src/main/java/edu/berkeley/grippus/storage/LocalFilesystemStorage.java
@@ -10,7 +10,6 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel.MapMode;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
