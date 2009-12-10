f08816718c3b60704758dee96fe4d8a8b98e73d9
Steven Schlansker <steven@sexy.stevenschlansker.is-a-geek.org>
2009-12-10 12:26:01 -0800

    Add storage blockFor

--- src/main/java/edu/berkeley/grippus/storage/Storage.java
+++ src/main/java/edu/berkeley/grippus/storage/Storage.java
@@ -3,12 +3,15 @@ package edu.berkeley.grippus.storage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 
 
 public interface Storage {
 	BlockList chunkify(File src) throws IOException;
 
 	InputStream readBlock(Block from) throws IOException;
-	
+
 	void createFile(byte[] digest, byte[] fileData) throws IOException;
+
+	Block blockFor(ByteBuffer buf) throws IOException;
 }
